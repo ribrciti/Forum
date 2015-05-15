53b9dc40f650a0ba6ca39d7961dfe31e9189cf0c
ribrciti <rbciti@msn.com>
2015-05-15 06:50:59 -0700

    Added delete and edit and associated views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,7 @@ class PostsController < ApplicationController
 		@posts = Post.all.order("created_at DESC")	
 	end
 
-	def show
-			
+	def show			
 	end
 
 	def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  resources :posts
+
+  # This creates nested routes by creating a "do" block
+  resources :posts do
+  	resources :comments
+  end
 
   root 'posts#index'  # This reads:  go to posts controller and do the method index
 end
